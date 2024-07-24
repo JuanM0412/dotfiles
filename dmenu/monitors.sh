53693f5a95d9f6ffcaddfa9c205261203bfbc8a1
JuanM0412 <juangomezp1204@gmail.com>
2024-07-24 15:22:18 -0500

    Volume status and dmenu color

--- dmenu/monitors.sh
+++ dmenu/monitors.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-options=$(echo -e "Extend\nSecond Screen\nMain Screen\nDuplicate\nOther" | dmenu -i -p "Output" -fn "Cantarell-13" -nb "#000000" -nf "#58D68D" -sb "#58D68D" -sf "#000000")
+options=$(echo -e "Extend\nSecond Screen\nMain Screen\nDuplicate\nOther" | dmenu -i -p "Output" -fn "Cantarell-13" -nb "#212F3D" -nf "#1793D1" -sb "#1793D1" -sf "#212F3D")
 
 case "$options" in
 	"Extend") /home/juan/.screenlayout/extend.sh ;;
