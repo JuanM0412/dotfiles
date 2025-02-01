92494768dd11a4d70cdd152c152e2eb16f88b35b
JuanM0412 <juangomezp1204@gmail.com>
2025-02-01 17:02:33 -0500

    Add installation script for essential packages and AUR helpers

---
+++ apps.sh
@@ -0,0 +1,16 @@
+sudo pacman -S --needed base-devel git terminator arandr xorg-setxkbmap libinput brightnessctl xf86-input-libinput feh picom pcmanfm pango ttf-font-awesome pacman-contrib procps-ng acpi lm_sensors spotify-launcher unzip flameshot
+
+git clone https://gist.github.com/fa6258f3ff7b17747ee3.git
+mv fa6258f3ff7b17747ee3/sp /usr/bin
+chmod +x /usr/bin/sp
+rm -rf fa6258f3ff7b17747ee3
+
+git clone https://aur.archlinux.org/yay-git.git
+cd yay-git/
+makepkg -si
+
+yay -S google-chrome pulseaudio-ctl
+
+git clone https://aur.archlinux.org/visual-studio-code-bin.git
+cd visual-studio-code-bin
+makepkg -si
