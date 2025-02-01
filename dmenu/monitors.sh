229084ffd298ca5be0fc079b8252c5aa80173de7
JuanM0412 <juangomezp1204@gmail.com>
2025-02-01 16:17:04 -0500

    Add screen management scripts and update i3 configuration

--- dmenu/monitors.sh
+++ dmenu/monitors.sh
@@ -3,9 +3,9 @@
 options=$(echo -e "Extend\nSecond Screen\nMain Screen\nDuplicate\nOther" | dmenu -i -p "Output" -fn "Cantarell-13" -nb "#212F3D" -nf "#1793D1" -sb "#1793D1" -sf "#212F3D")
 
 case "$options" in
-	"Extend") /home/juan/.screenlayout/extend.sh ;;
-	"Second Screen") /home/juan/.screenlayout/second.sh ;;
-	"Main Screen") /home/juan/.screenlayout/main.sh ;;
-	"Duplicate") /home/juan/.screenlayout/duplicate.sh ;;
+	"Extend") /home/juan/.config/screen/extend.sh ;;
+	"Second Screen") /home/juan/.config/screen/second.sh ;;
+	"Main Screen") /home/juan/.config/screen/main.sh ;;
+	"Duplicate") /home/juan/.config/screen/duplicate.sh ;;
 	"Other") arandr ;;
 esac
